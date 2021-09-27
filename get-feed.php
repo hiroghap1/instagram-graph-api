7c423d6a2bb9c77003bebeb188eddbbb7486cf80
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2021-09-28 06:09:43 +0900

    feat():除外ファイル設定

--- get-feed.php
+++ get-feed.php
@@ -1,7 +1,7 @@
 <?php
+include_once ('api-setting.php');
+
 $version = 'v12.0';
-$instagramBusinessAccount = '';
-$access_token = '';
 $url = 'https://graph.facebook.com/'.$version.'/'.$instagramBusinessAccount.'?fields=name%2Cmedia.limit(9)%7Bcaption%2Cmedia_url%2Cthumbnail_url%2Cpermalink%2Ctimestamp%7D&access_token='.$access_token;
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
